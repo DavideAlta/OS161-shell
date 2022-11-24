8c596258f1d512f40a4a2425c267b45742d0e457
DavideAlta <davidealtamore12@gmail.com>
2022-11-24 16:52:55 +0100

    fixed execv (args not properly read)

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -223,12 +223,15 @@ int sys__exit(int exitcode){
 
 int sys_execv(char *program, char **args){
 
-    int err;
+    int err, argc=0, padding=0;
+    int args_size=0; // Total size of kernel buffer (args size + pointers size)
     struct vnode *vn;
     struct addrspace *as;
     vaddr_t entrypoint, stackptr;
     size_t arglen=0;
-    int padding=0;
+    char **kargs; // To move inside kernel buffer (string by string) -> i.e. kernel buffer
+    char *kargs_ptr; // To move inside kernel buffer (char by char)
+    char *kargs_ptr_start; // Starting position of arguments inside kernel buffer
 
     // Check arguments validity
     if((program == NULL) || (args == NULL)) {
@@ -239,13 +242,11 @@ int sys_execv(char *program, char **args){
 
     // The n. of elements of args[] is unknown but the last argument should be NULL.
     // Compute argc = n. of valid arguments in args[]
-    int argc = 0;
-    int args_size = 0; // Total size of the argument strings
     while(args[argc] != NULL){
         // Accumulate the size of each args[] element
         arglen = strlen(args[argc])+1;
         args_size += sizeof(char)*(arglen);
-        // Compute n. of 0s for padding
+        // Compute n. of 0s for padding (if needed)
         if((arglen%4) != 0){
             padding = 4 - arglen%4;
             args_size += padding;
@@ -268,34 +269,55 @@ int sys_execv(char *program, char **args){
         return ENOMEM;
     }
 
-    // - Single arguments
-    args_size += 4*(argc+1); // update the tot size with pointers dimensions
-    char *kargs = (char *)kmalloc(args_size); // array of char (= 1 byte) [kernel buffer]
-    // The starting position is over the arg pointers (each pointer is a 4bytes integer)
-    int cur_pos = 4*(argc+1); // 16 (due to 3 args + 1 null)
-    int arg_pointers[argc+1]; // Offsets of the arguments in the buffer
-    // Suppose to have these arguments: "foo\0" "hello\0" "1\0"
-    for(int i=0;i<argc;i++){
-        // Move args to kernel space
-        err = copyinstr((const_userptr_t)args[i], &kargs[cur_pos] /*(char*)(kargs+cur_pos)*/, ARG_MAX, &arglen); // arglen = 4 (foo\0) 6 (hello\0) 2 (1\0)
+    // - Single arguments with their pointers
+
+    // Update the tot size to can insert the pointers
+    args_size = args_size + sizeof(char *)*(argc+1);
+
+    // Allocate the kernel buffer (choose a pointer inside kernel heap)
+    kargs = (char **)kmalloc(args_size);
+
+    // The starting position is over the arg pointers
+    kargs_ptr_start = (char *)(kargs + argc + 1); // e.g. A + 16
+
+    // Initialize the pointer to move inside kernel buffer
+    kargs_ptr = kargs_ptr_start;
+
+    // Fill the kernel buffer
+    for(int i=0;i<argc;i++){ // e.g. the args are "foo\0" "hello!\0" "1\0"
+        
+        // - Copy the arguments pointers into kernel buffer
+        kargs[i] = kargs_ptr;
+
+        // - Copy the arguments into kernel buffer
+        err = copyinstr((const_userptr_t)args[i], kargs_ptr, ARG_MAX, &arglen); // e.g. arglen = 4, 7, 2
         if(err){
             return err;
         }
-        arg_pointers[i] = cur_pos;
-        cur_pos += arglen; // cur_pos =  16 > 20 > 26 > 30
-        // if the argument string has no exactly 4 bytes '\0'-padding is needed
+
+        // Move pointer on the copied argument
+        kargs_ptr += arglen;
+
+        // If the argument string has no exactly 4 bytes 0-padding is needed
         if((arglen%4) != 0){
-            // n. of bytes to pad at '\0'
-            padding = 4 - arglen%4; // padding = 2 > 2
+            // n. of bytes to pad
+            padding = 4 - arglen%4;
             for(int j=0;j<padding;j++){
-                kargs[cur_pos + j] = '\0';
+                // Move on the pointer by following the 0s added
+                kargs_ptr += j; 
+                // Add the \0
+                memcpy(kargs_ptr,"\0",1);
+                //strcat(kargs_ptr,"\0"); // concatenate a \0 char
             }
-            cur_pos += padding; // cur_pos = 28 > 32
         }
     }
     // Now kargs[] is the copy of args in kernel space with the proper padding
+    // and kargs_ptr points to the last char of kernel buffer
 
-    /* Create a new address space and load the executable into it
+    // Set the last pointer to null
+    kargs[argc] = NULL;
+
+    /* 3. Create a new address space and load the executable into it
      * (same of runprogram) */
 
     // Open the "program" file
@@ -332,34 +354,18 @@ int sys_execv(char *program, char **args){
 		return err;
 	}
 
-    // Start position of the stack
-    stackptr -= cur_pos;
+    /* 4. Copy the kernel buffer into stack (remember to update the pointers) */
 
-    // Current position of the stack
-    int stackpos;
+    // Start position of the stack (= allocate into stack the same size of kernel buffer to copy)
+    stackptr -= args_size;
 
-    // Copy the arguments args in the stack
+    // Update the pointers in the kernel buffer with the starting stack position
     for(int i=0;i<argc;i++){
-        // Update current position of the stack (arg_pointers behave like offsets for stackptr)
-        stackpos = stackptr + arg_pointers[i];
-        // Copy i-th element of kargs array into the stack at the computed position
-        // (the length, considering the padding, is provided by the distance between 2 offsets)
-        copyout(&kargs[i], (userptr_t)stackpos, arg_pointers[i+1]-arg_pointers[i]);
+        kargs[i] = kargs[i] - kargs_ptr_start + (char *)stackptr;
     }
 
-    // Update the pointers array with the starting stack position
-    for(int i=0;i<argc;i++){
-        arg_pointers[i] = arg_pointers[i] + stackptr;
-    }
-    // Copy the updated pointers from kernel buffer to user stack
-    copyout(arg_pointers, (userptr_t)stackptr, 4*argc);
-    // if no work kmalloccare
-
-    // The last pointer is set to NULL
-    /*stackptr += 4*argc; 
-    *stackptr = NULL;
-    stackptr -= 4*argc;*/
-
+    // Copy the whole kernel buffer into user stack
+    copyout(kargs, (userptr_t)stackptr, args_size);
 
     enter_new_process(argc /*argc*/, (userptr_t)stackptr /*userspace addr of argv*/,
 			  NULL /*userspace addr of environment*/,
